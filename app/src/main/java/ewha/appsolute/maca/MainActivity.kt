34385138573bc4c97ced609c5f5e3d3544cc4f80
Yerin Hwang <hdan3653@gmail.com>
2020-07-11 00:07:22 +0900

    Add test data for main recycler view

--- app/src/main/java/ewha/appsolute/maca/MainActivity.kt
+++ app/src/main/java/ewha/appsolute/maca/MainActivity.kt
@@ -2,10 +2,10 @@ package ewha.appsolute.maca
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -33,22 +33,14 @@ class MainActivity : AppCompatActivity() {
             WordDB::class.java, "database-name"
         ).build()
 
-        var test = Word(0, "apple", POS.NOUN.ordinal
-            , "사과", "사과의 긴 뜻을 표현하고자 주저리 주저리\n그리고 어쩌구 저쩌구."
-            , false, 0, "2020-07-10", null
-        )
-
         var wordList = WordList()
-        wordList.addWord(test)
-        wordList.addWord(test)
-        wordList.addWord(test)
-
+        testData(wordList)                          //TEMP
         wordList.printWordList()
 
-//        val adapter = VocaCardAdapter(wordList)
-//
-//        vocaCardView.adapter = adapter
-//        vocaCardView.layoutManager = GridLayoutManager(this, 2)
+        val adapter = VocaCardAdapter(wordList)
+
+        vocaCardView.adapter = adapter
+        vocaCardView.layoutManager = GridLayoutManager(this, 2)
     }
 
     fun vocaCardPopup() {
@@ -60,5 +52,76 @@ class MainActivity : AppCompatActivity() {
 
         alertDialog.setView(view)
         alertDialog.show()
+    }
+
+    private fun testData(wordList: WordList) {
+        wordList.addWord(Word(0, "apple", POS.NOUN.ordinal
+            , "사과", "사과의 긴 뜻을 표현하고자 주저리 주저리\n그리고 어쩌구 저쩌구."
+            , false, 0, "2020-06-10", null
+        ))
+        wordList.addWord(Word(0, "banana", POS.NOUN.ordinal
+            , "바나나", "노랗다"
+            , false, 0, "2020-06-10", null
+        ))
+        wordList.addWord(Word(0, "caramel", POS.NOUN.ordinal
+            , "캐러맬", null
+            , false, 0, "2020-07-03", null
+        ))
+        wordList.addWord(Word(0, "dinosaur", POS.NOUN.ordinal
+            , "공룡", null
+            , false, 0, "2020-07-10", null
+        ))
+        wordList.addWord(Word(0, "airplane", POS.NOUN.ordinal
+            , "비행기", null
+            , false, 0, "2020-07-10", null
+        ))
+        wordList.addWord(Word(0, "phone", POS.NOUN.ordinal
+            , "전화", null
+            , false, 0, "2020-07-10", null
+        ))
+        wordList.addWord(Word(0, "aphasia", POS.NOUN.ordinal
+            , "실어증", null
+            , false, 0, "2020-07-10", null
+        ))
+        wordList.addWord(Word(0, "cake", POS.NOUN.ordinal
+            , "케이크", null
+            , false, 0, "2020-07-10", null
+        ))
+        wordList.addWord(Word(0, "fox", POS.NOUN.ordinal
+            , "여우", null
+            , false, 0, "2020-07-10", null
+        ))
+        wordList.addWord(Word(0, "book", POS.NOUN.ordinal
+            , "책", null
+            , false, 0, "2020-07-10", null
+        ))
+        wordList.addWord(Word(0, "note", POS.NOUN.ordinal
+            , "공책", null
+            , false, 0, "2020-07-10", null
+        ))
+        wordList.addWord(Word(0, "pen", POS.NOUN.ordinal
+            , "펜", null
+            , false, 0, "2020-07-10", null
+        ))
+        wordList.addWord(Word(0, "patriarchal", POS.ADJECTIVE.ordinal
+            , "가부장적인", null
+            , false, 0, "2020-07-10", null
+        ))
+        wordList.addWord(Word(0, "tie", POS.VERB.ordinal
+            , "묶다", "다른 뜻도 있다."
+            , false, 0, "2020-07-10", null
+        ))
+        wordList.addWord(Word(0, "discreetly", POS.ADVERB.ordinal
+            , "신중히", null
+            , false, 0, "2020-07-10", null
+        ))
+        wordList.addWord(Word(0, "neath", POS.PREPOSITION.ordinal
+            , "아래에", null
+            , false, 0, "2020-07-10", null
+        ))
+        wordList.addWord(Word(0, "take one's place", POS.IDIOM.ordinal
+            , "자리를 차지하다", null
+            , false, 0, "2020-07-10", null
+        ))
     }
 }
--- app/src/main/java/ewha/appsolute/maca/WordList.kt
+++ app/src/main/java/ewha/appsolute/maca/WordList.kt
@@ -15,8 +15,9 @@ class WordList () {
     fun printWordList() {
         for(i in 0..index.lastIndex) {
             var val1 = index[i]
-            var val2 = wordlist[i].voca
-            Log.println(Log.DEBUG, "", "$i : $val1 || $val2")
+            var id = wordlist[i].id
+            var voca = wordlist[i].voca
+            Log.println(Log.DEBUG, "", "$i\t:\tindex[$i] = $val1\t||\tid=$id\t$voca")
         }
     }
 
