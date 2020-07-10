c6e38b50fedefb814efa7bf6986a824cdba3b529
Yerin Hwang <hdan3653@gmail.com>
2020-07-10 17:10:23 +0900

    word database initialize

--- app/src/main/java/ewha/appsolute/maca/Word.kt
+++ app/src/main/java/ewha/appsolute/maca/Word.kt
@@ -1,3 +1,24 @@
 package ewha.appsolute.maca
 
-class Word
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
+import java.util.*
+
+enum class POS{
+    NOUN, ADJECTIVE, VERB, ADVERB, PREPOSITION, IDIOM
+}
+
+@Entity(tableName = "Word")
+data class Word(
+    @PrimaryKey(autoGenerate = true) val id: Long
+    , @ColumnInfo(name="WORD") @NotNull var word: String
+    , @ColumnInfo(name="POS") @NotNull var pos: POS
+    , @ColumnInfo(name="PRIMARY") @NotNull var primary: String
+    , @ColumnInfo(name="SECONDARY") var secondary: String
+    , @ColumnInfo(name="IS_SAVED") @NotNull var isSaved: Boolean = false
+    , @ColumnInfo(name="COUNT_MEM") @NotNull var count_mem: Int = 0
+    , @ColumnInfo(name="DATE_ADD") @NotNull var date_add: Date
+    , @ColumnInfo(name="DATE_MEM") var date_mem: Date
+)
--- app/src/main/java/ewha/appsolute/maca/WordList.kt
+++ app/src/main/java/ewha/appsolute/maca/WordList.kt
@@ -1,3 +1,6 @@
 package ewha.appsolute.maca
 
-class WordList (val voca: String, meaning: String, part: String, correct: Int)
+class WordList (val voca: String, meaning: String, part: String, correct: Int) {
+    lateinit var list: ArrayList<Word>
+    lateinit var index: List<Int>
+}
