dae8a055befb9b5e2138d1e8b7a3abab0c0b6d7b
Yerin Hwang <hdan3653@gmail.com>
2020-07-15 00:50:31 +0900

    Merge & Edited CardView Layout

--- app/src/main/java/ewha/appsolute/maca/VocaCardAdapter.kt
+++ app/src/main/java/ewha/appsolute/maca/VocaCardAdapter.kt
@@ -1,17 +1,14 @@
 package ewha.appsolute.maca
 
 import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.item_vocacard.view.*
-import java.security.AccessController.getContext
 
 //list 데이터 타입 수정함
 
-class VocaCardAdapter(val list: WordList,  val context: Context): RecyclerView.Adapter<VocaCardViewHolder>(){
+class VocaCardAdapter(val list: WordList, val context:Context): RecyclerView.Adapter<VocaCardViewHolder>(){
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VocaCardViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_vocacard,parent,false)
         return VocaCardViewHolder(view)
--- app/src/main/java/ewha/appsolute/maca/VocaCardViewHolder.kt
+++ app/src/main/java/ewha/appsolute/maca/VocaCardViewHolder.kt
@@ -1,15 +1,10 @@
 package ewha.appsolute.maca
 
 import android.content.Context
-import android.content.Intent
 import android.view.View
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_vocacard.view.*
-import java.security.AccessController
-import java.security.AccessController.getContext
 
 class VocaCardViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
     LayoutContainer{
